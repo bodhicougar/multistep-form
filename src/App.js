47f5a78b46ac3f4bb24ff1ddde3758568d136dfc
bodhicougar <github@hoosnagh.us>
2018-10-11 12:33:40 +0200

    First commit
    Signed-off-by: bodhicougar <github@hoosnagh.us>

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+import MainForm from './components/MainForm';
+import {
+  Container
+} from 'semantic-ui-react';
+
+class App extends Component {
+
+  render() {
+    return ( <
+      Container textAlign = 'center' >
+      <
+      MainForm / >
+      <
+      /Container>     )
+    }
+  }
+
+  export default App;
